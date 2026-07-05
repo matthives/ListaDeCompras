0b76ea69898519072c269a4f17c284704212673c
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 20:20:07 -0300

    Atualizado RepositorioBase

--- ListaDeCompras.ConsoleApp/Compartilhado/RepositorioBase.cs
+++ ListaDeCompras.ConsoleApp/Compartilhado/RepositorioBase.cs
@@ -2,18 +2,11 @@ namespace ListaDeCompras.ConsoleApp.Compartilhado;
 
 public abstract class RepositorioBase<TEntidade> where TEntidade : EntidadeBase
 {
-    private readonly TEntidade[] registros = new TEntidade[100];
+    private readonly List<TEntidade> registros = new List<TEntidade>();
 
     public void Cadastrar(TEntidade novoRegistro)
     {
-        for (int i = 0; i < registros.Length; i++)
-        {
-            if (registros[i] == null)
-            {
-                registros[i] = novoRegistro;
-                break;
-            }
-        }
+        registros.Add(novoRegistro);
     }
 
     public bool Editar(int idSelecionado, TEntidade entidadeAtualizada)
@@ -30,32 +23,18 @@ public abstract class RepositorioBase<TEntidade> where TEntidade : EntidadeBase
 
     public bool Excluir(int idSelecionado)
     {
-        for (int i = 0; i < registros.Length; i++)
-        {
-            TEntidade o = registros[i];
+        TEntidade? registro = SelecionarPorId(idSelecionado);
 
-            if (o == null)
-                continue;
+        if (registro == null)
+            return false;
 
-            if (o.Id == idSelecionado)
-            {
-                registros[i] = null;
-                return true;
-            }
-        }
-
-        return false;
+        return registros.Remove(registro);
     }
 
     public TEntidade? SelecionarPorId(int idSelecionado)
     {
-        for (int i = 0; i < registros.Length; i++)
+        foreach (TEntidade o in registros)
         {
-            TEntidade o = registros[i];
-
-            if (o == null)
-                continue;
-
             if (o.Id == idSelecionado)
                 return o;
         }
@@ -63,7 +42,7 @@ public abstract class RepositorioBase<TEntidade> where TEntidade : EntidadeBase
         return null;
     }
 
-    public TEntidade[] SelecionarTodos()
+    public List<TEntidade> SelecionarTodos()
     {
         return registros;
     }
