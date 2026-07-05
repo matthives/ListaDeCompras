0247e4653be55a5210586045097ad614ab0e1661
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 20:53:21 -0300

    Atualizado RepositorioProduto

--- ListaDeCompras.ConsoleApp/Modulos/ModuloProduto/RepositorioProduto.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloProduto/RepositorioProduto.cs
@@ -1,7 +1,16 @@
 using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.Compartilhado.Arquivos;
 
 namespace ListaDeCompras.ConsoleApp.Modulos.ModuloProduto;
 
 public class RepositorioProduto : RepositorioBase<Produto>
 {
+    public RepositorioProduto(ContextoJson contexto) : base(contexto)
+    {
+    }
+
+    protected override List<Produto> ObterRegistros()
+    {
+        return contexto.Produtos;
+    }
 }
