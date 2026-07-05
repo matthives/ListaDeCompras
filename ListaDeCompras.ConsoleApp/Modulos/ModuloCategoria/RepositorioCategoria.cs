a9765b1b9174eaae70cfa879edd4988bd0f4e832
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 20:42:51 -0300

    Atualizado RepositorioCategoria

--- ListaDeCompras.ConsoleApp/Modulos/ModuloCategoria/RepositorioCategoria.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloCategoria/RepositorioCategoria.cs
@@ -1,7 +1,16 @@
 using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.Compartilhado.Arquivos;
 
 namespace ListaDeCompras.ConsoleApp.Modulos.ModuloCategoria;
 
 public class RepositorioCategoria : RepositorioBase<Categoria>
 {
+    public RepositorioCategoria(ContextoJson contexto) : base(contexto)
+    {
+    }
+
+    protected override List<Categoria> ObterRegistros()
+    {
+        return contexto.Categorias;
+    }
 }
