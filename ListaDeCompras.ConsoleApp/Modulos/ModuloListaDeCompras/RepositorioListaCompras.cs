58251e7e6c20e8c656244245488accf3c80e1c4b
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 20:14:27 -0300

    Atualizado RepositorioListaCompras

--- ListaDeCompras.ConsoleApp/Modulos/ModuloListaDeCompras/RepositorioListaCompras.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloListaDeCompras/RepositorioListaCompras.cs
@@ -1,7 +1,7 @@
 using ListaDeCompras.ConsoleApp.Compartilhado;
 
-namespace ListaDeCompras.ConsoleApp.Modulos.ModuloCategoria;
+namespace ListaDeCompras.ConsoleApp.Modulos.ModuloListaCompras;
 
-public class RepositorioListaCompras : RepositorioBase
+public class RepositorioListaCompras : RepositorioBase<ListaCompras>
 {
 }
