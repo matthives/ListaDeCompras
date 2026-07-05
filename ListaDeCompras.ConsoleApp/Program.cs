54df1cdaeced2f830684c433640b53632773285d
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 19:50:09 -0300

    Atualizado Program

--- ListaDeCompras.ConsoleApp/Program.cs
+++ ListaDeCompras.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.Modulos.ModuloListaCompras;
 
 TelaPrincipal telaPrincipal = new TelaPrincipal();
 
@@ -29,6 +30,18 @@ while (true)
 
             else if (opcaoMenuInterno == "4")
                 telaBase.VisualizarTodos(true);
+
+            if (telaBase is TelaListaCompras telaListaCompras)
+            {
+                if (opcaoMenuInterno == "5")
+                    telaListaCompras.AdicionarItem();
+
+                else if (opcaoMenuInterno == "6")
+                    telaListaCompras.RemoverItem();
+
+                else if (opcaoMenuInterno == "7")
+                    telaListaCompras.VisualizarItens(true);
+            }
         }
     }
 }
