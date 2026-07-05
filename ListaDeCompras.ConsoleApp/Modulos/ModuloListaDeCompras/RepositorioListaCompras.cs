0e15d5fdd36980a2e2a9ce0fa46c1b7089ad5bfe
Mateus Thives <mateus.thives@hotmail.com>
2026-07-05 20:47:13 -0300

    Atualizado RepositorioListaCompras

--- ListaDeCompras.ConsoleApp/Modulos/ModuloListaDeCompras/RepositorioListaCompras.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloListaDeCompras/RepositorioListaCompras.cs
@@ -1,7 +1,16 @@
 using ListaDeCompras.ConsoleApp.Compartilhado;
+using ListaDeCompras.ConsoleApp.Compartilhado.Arquivos;
 
 namespace ListaDeCompras.ConsoleApp.Modulos.ModuloListaCompras;
 
 public class RepositorioListaCompras : RepositorioBase<ListaCompras>
 {
+    public RepositorioListaCompras(ContextoJson contexto) : base(contexto)
+    {
+    }
+
+    protected override List<ListaCompras> ObterRegistros()
+    {
+        return contexto.ListasDeCompras;
+    }
 }
